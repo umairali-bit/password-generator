7308ef28f943121145f86d1410dbc6363371c305
Umair Ali <umairmamoor@gmail.com>
2021-07-09 00:15:14 -0400

    new arrays

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,10 @@
 
 
 //Arrays containing all the characters needed.
-var Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var SpecialChars = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~",];
-var AlphaLowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-var AlphaUppers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var Numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var SpecialChars = ['!', '@', '#', '$', '%','^', '&', '*', '(', ')','{', ' }', '[', ']', '~','_', '-', '.', ';', ':',',', '\\'];
+var AlphaLowers = ['a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't','u', 'v', 'w', 'x', 'y','z'];
+var AlphaUppers = [  'A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O','P', 'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y','Z'];
 
 
 //main function
